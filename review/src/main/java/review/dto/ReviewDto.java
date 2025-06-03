500aa6822748ab646c9b6bbeede5ba8b29f07273
Suhyun An <movptr06@outlook.kr>
2025-06-03 16:56:30 +0900

    refactor: DTO 관련 코드 스타일 수정

--- review/src/main/java/review/dto/ReviewDto.java
+++ review/src/main/java/review/dto/ReviewDto.java
@@ -6,14 +6,17 @@ import jakarta.validation.constraints.NotNull;
 public class ReviewDto {
 
   public record Request(@NotNull Float score, String comment) {
+
   }
 
   public record Command(String reviewee, String reviewer, Float score, String comment) {
+
   }
 
   public record Response(List<Review> Reviews) {
 
     public record Review(Long id, String reviewee, String reviewer, Float score, String comment) {
+
     }
 
   }
--- review/src/main/java/review/dto/ScoreDto.java
+++ review/src/main/java/review/dto/ScoreDto.java
@@ -3,6 +3,7 @@ package review.dto;
 public class ScoreDto {
 
   public record Response(Float score) {
+
   }
 
 }
